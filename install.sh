2852af1961c3ed87d0bb841d035b04bd10c14bea
techrisdev <techrisdev>
2021-12-04 19:55:24 +0100

    Initial Commit

---
+++ install.sh
@@ -0,0 +1,4 @@
+# I want st to be installed in ~/.bin
+sudo rm -f /usr/local/bin/st
+make st
+sudo cp st /usr/local/bin/st
---
+++ install_user.sh
@@ -0,0 +1,4 @@
+# I want st to be installed in ~/.bin
+rm -f ~/.bin/st
+make st
+cp st ~/.bin
